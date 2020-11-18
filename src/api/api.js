d90621c7e58588fa92fb00cd85916703239a57b3
hesai1 <sai.he1@pactera.com>
2020-11-18 11:20:34 +0900

    change url of api gateway

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 export default {
   instance () {
     var api = axios.create({
-      baseURL: 'https://c1vdx9kkf4.execute-api.ap-northeast-1.amazonaws.com/dev',
+      baseURL: 'https://w6iy4rqrfk.execute-api.ap-northeast-1.amazonaws.com/dev',
       // withCredentials: true,
       // 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'
       // headers: {
